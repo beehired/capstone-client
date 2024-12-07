212669bef68cc9bc8c13b8098cd43a64133f3115
joshuaramin <raminjoshua05@gmail.com>
2024-12-08 02:49:01 +0800

    remove unused declaration

--- components/dashboard/applicant/profile/profileTab/design/card.tsx
+++ components/dashboard/applicant/profile/profileTab/design/card.tsx
@@ -2,29 +2,10 @@ import Image from 'next/image'
 import React from 'react'
 import styles from '@/styles/dashboard/applicant/profile/module/design.card.module.scss';
 import { RegularPoppins } from '@/components/typograhy';
-import { useQuery } from '@tanstack/react-query';
-import { GraphQLRequest } from '@/lib/graphQLRequest';
-import { GetUserByProfileId } from '@/util/Query/profile.query';
 import cn from '@/util/cn';
-import store from 'store2';
 
 
 export default function Card({ id, name, image, s, handleChange, value }: { id: string, name: string, image: string, s: string, handleChange: any, value: any }) {
-
-
-    const user = store.get("UserAccount");
-
-    const { data, isLoading } = useQuery({
-        queryKey: ["GetUserProfileById", user?.user?.myProfile.profileID],
-        queryFn: async () => {
-            const { getUserProfileById } = await GraphQLRequest(GetUserByProfileId, {
-                profileId: user?.user?.myProfile.profileID
-            })
-
-            return getUserProfileById
-        },
-    })
-
 
     return (
         <button aria-label={name} value={id} name={s} onClick={(e) => handleChange(s, id)} className={
